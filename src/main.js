9327ef8f396979a28998c3b537cd992a238f6d64
Carlos Roberto <crpu4a@gmail.com>
2021-07-01 21:47:08 -0300

    feat: instalçao e configuração do axio e re-captcha-3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import { createApp } from 'vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 import router from './router.js'
 
 const app = createApp(App)
+
 app.use(router)
+app.use(VueAxios, axios)
+app.use(VueReCaptcha, {
+  siteKey: '6Ld3cd0UAAAAAEtD4rnSFxLQjJTMRM-J-aGfkMnK',
+  loaderOptions: {
+    useRecaptchaNet: true,
+    autoHideBadge: true,
+  },
+})
+
 app.mount('#app')
