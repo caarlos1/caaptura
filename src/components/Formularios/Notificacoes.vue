07f564d0073d8cb0b81a64532f774d3ddaad1194
Carlos Roberto <crpu4a@gmail.com>
2021-07-14 19:31:05 -0300

    refactor: componetização de Notificações do formulário de Orçamento

---
+++ src/components/Formularios/Notificacoes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="temNotificacoes" class="col-md-12 formulario__notificacoes">
+    <ul>
+      <li
+        v-for="notificacao in notificacoes"
+        :key="notificacao"
+        v-bind:class="{
+          'formulario__notificacao--sucesso': notificacao.sucesso,
+          'formulario__notificacao--erro': !notificacao.sucesso,
+        }"
+        class="alert alert-dark"
+        role="alert"
+      >
+        {{ notificacao.mensagem }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { FormualarioNotificacoes } from './IFormulario'
+
+export default defineComponent({
+  props: {
+    notificacoes: Array,
+  },
+
+  methods: {
+    temNotificacoes(): boolean {
+      return this.notificacoes?.length != 0
+    },
+  },
+
+  computed: {
+    foiSucesso(notificacao: FormualarioNotificacoes): boolean {
+      return notificacao.sucesso
+    },
+  },
+})
+</script>
