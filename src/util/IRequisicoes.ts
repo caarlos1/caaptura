b23d4fb69cf1d065c5006b28573a4c7bb08cfcef
Carlos Roberto <crpu4a@gmail.com>
2021-07-14 19:26:09 -0300

    feat: edição da pasta útil com criação de classe Util e interface das requisições

---
+++ src/util/IRequisicoes.ts
@@ -0,0 +1,5 @@
+export interface ResultadoRequisicao {
+  sucesso: boolean
+  conteudo?: unknown
+  alertas: string[]
+}
---
+++ src/util/index.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+export function isDevMode(): boolean {
+  return process.env.NODE_ENV === 'development'
+}
+
+export function montarTituloPagina(...trechos: string[]): string {
+  let titulo = ''
+  trechos.forEach(texto => {
+    titulo += texto
+  })
+  return titulo
+}
+
+export function redirectErroURL(): void {
+  if (!isDevMode()) window.location.href = process.env.VUE_APP_URL_REDIRECT_ERRO
+}
+
+export function notificacaoFormularioFactory(
+  mensagem: string,
+  sucesso: boolean
+) {
+  return {
+    mensagem,
+    sucesso,
+  }
+}
+
+export class Util {
+  public static emailValido(email: string): boolean {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email)
+  }
+
+  public static obterUrlPagina(): string {
+    return `${window.location.href}`
+  }
+
+  public static obterHostnamePagina(): string {
+    return `${window.location.hostname}`
+  }
+
+  public static async obterIpUsuario(): Promise<string> {
+    try {
+      const { data } = await axios.get('https://api.ipify.org?format=json')
+      return data.ip
+    } catch (err) {
+      return ''
+    }
+  }
+}
